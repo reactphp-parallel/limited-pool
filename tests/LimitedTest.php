67696220994970f180c78c42d2cf18e9956bfe9b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2024-01-07 21:18:23 +0100

    PHP 8.2+

--- tests/LimitedTest.php
+++ tests/LimitedTest.php
@@ -1,9 +1,9 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace ReactParallel\Tests\Pool\Limited;
 
-use React\EventLoop\Factory;
-use React\EventLoop\LoopInterface;
 use ReactParallel\Contracts\PoolInterface;
 use ReactParallel\EventLoop\EventLoopBridge;
 use ReactParallel\Pool\Infinite\Infinite;
@@ -12,21 +12,18 @@ use ReactParallel\Tests\AbstractPoolTest;
 use WyriHaximus\PoolInfo\PoolInfoInterface;
 use WyriHaximus\PoolInfo\PoolInfoTestTrait;
 
-/**
- * @internal
- */
+/** @internal */
 final class LimitedTest extends AbstractPoolTest
 {
     use PoolInfoTestTrait;
 
     private function poolFactory(): PoolInfoInterface
     {
-        $loop = Factory::create();
-        return new Limited(new Infinite($loop, new EventLoopBridge($loop), 1), 5);
+        return new Limited(new Infinite(new EventLoopBridge(), 1), 5);
     }
 
-    protected function createPool(LoopInterface $loop): PoolInterface
+    protected function createPool(): PoolInterface
     {
-        return new Limited(new Infinite($loop, new EventLoopBridge($loop), 1), 5);
+        return new Limited(new Infinite(new EventLoopBridge(), 1), 5);
     }
 }

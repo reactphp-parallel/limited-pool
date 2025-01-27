c0cc5306d0ba2896b02d718a46ecd8c9ec64eb04
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-01-27 17:48:13 +0100

    [2.x] Improve template types

---
+++ tests/LimitedTestTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReactParallel\Tests\Pool\Limited;
+
+use React\EventLoop\Loop;
+use React\Promise\Deferred;
+use ReactParallel\EventLoop\EventLoopBridge;
+use ReactParallel\Pool\Infinite\Infinite;
+use ReactParallel\Pool\Limited\Limited;
+use Throwable;
+
+use function range;
+use function React\Async\async;
+use function React\Async\await;
+use function React\Promise\all;
+
+trait LimitedTestTrait
+{
+    /** @test */
+    public function assertWeCanRunMoreThanThePoolLimit(): void
+    {
+        $pool = new Limited(new Infinite(new EventLoopBridge(), 1), 5);
+
+        $is = [];
+        foreach (range(1, 1337) as $i) {
+            $deferred = new Deferred();
+            Loop::futureTick(async(static function () use ($pool, $deferred, $i): void {
+                try {
+                    $deferred->resolve($pool->run(static function () use ($i): int {
+                        return $i;
+                    }));
+                } catch (Throwable $exception) {
+                    $deferred->reject($exception);
+                }
+            }));
+            $is[] = $deferred->promise();
+        }
+
+        self::assertSame(range(1, 1337), await(all($is)));
+    }
+}
--- tests/LimitedWithPoolTest.php
+++ tests/LimitedWithPoolTest.php
@@ -1,5 +1,6 @@
 <?php
 
+// phpcs:disable
 declare(strict_types=1);
 
 namespace ReactParallel\Tests\Pool\Limited;
@@ -9,11 +10,14 @@ use ReactParallel\EventLoop\EventLoopBridge;
 use ReactParallel\Pool\Infinite\Infinite;
 use ReactParallel\Pool\Limited\Limited;
 use ReactParallel\Tests\AbstractPoolTest;
+use WyriHaximus\AsyncTestUtilities\TimeOut;
 //use WyriHaximus\PoolInfo\PoolInfoInterface;
 //use WyriHaximus\PoolInfo\PoolInfoTestTrait;
 
+#[TimeOut(13)]
 final class LimitedWithPoolTest extends AbstractPoolTest
 {
+    use LimitedTestTrait;
 //    use PoolInfoTestTrait;
 //
 //    private function poolFactory(): PoolInfoInterface
